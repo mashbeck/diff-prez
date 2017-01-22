76825beff1f888a5c2d61815ef4a649487cd1e79
Matthew <mashbeck@purdue.edu>
2017-01-22 02:07:19 -0500

    Added SQL connection code

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
+var mysql = require('mysql');
 var express = require('express');
 var bodyparser = require('body-parser');
 var app = express();
+
+/*var Oconnection = mysql.createConnection({
+ host	: 'localhost',
+ user	: 'root',
+ password : '',
+ database : 'ObamaGov',
+});
+
+var Tconnection = mysql.createConnection({
+	host	: 'localhost',
+	user	: 'root',
+	password : '',
+	database : 'TrumpGov',
+});
+
+Oconnection.connect();
+Tconnection.connect(); */
 
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static('site'));
