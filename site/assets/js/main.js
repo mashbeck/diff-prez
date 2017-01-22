628e8f5b1a89db9cfd3eeb13d944bb6f54308ce4
Jonathan Grider <jagrider@purdue.edu>
2017-01-22 02:46:45 -0500

    update - changed js, updated styles, about

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -3,13 +3,5 @@
 */
 $(function()) {
   //TODO: FIND THINGS TO DO
-  var forms = document.getElementsByTagName('formID');
-  for (var i = 0; i < forms.length; i++) {
-    forms[i].noValidate = true;
-    forms[i].addEventListener('submit', function(event) {
-      if (!event.target.checkValidity()) {
-        event.preventDefault();
-      }
-    }, false);
-  }
+  
 });
