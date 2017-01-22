868fea9839aa2bff9c5d7481b80ef27d314faa7c
Jonathan Grider <jagrider@purdue.edu>
2017-01-22 02:18:54 -0500

    added - attempt at scripting no resonse

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -3,4 +3,14 @@
 */
 $(function()) {
   //TODO: FIND THINGS TO DO
+  var form = document.getElementById('formID');
+  form.noValidate = true;
+  form.addEventListener('submit', function(event)) {
+    if (!event.target.checkValidity()) {
+      event.preventDefault();
+      document.getElementById('errorMessageDiv').style.display = 'block';
+    }
+
+  }
+
 });
