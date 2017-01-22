40fcee7abd58c76db9c5701501b7831ed2b4179f
Jonathan Grider <jagrider@purdue.edu>
2017-01-22 02:56:57 -0500

    update - added scrolling to about section

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -1,7 +1,32 @@
 /*
-  Do these things when the page loads
+* Stuff that needs done when the page loads.
 */
-$(function()) {
-  //TODO: FIND THINGS TO DO
-  
+$(function() {
+  registerNavButtons();
 });
+
+/*
+* registerNavButtons lets us register the buttons for about and contact
+*/
+function registerNavButtons() {
+  var sections = ["about"];
+
+  sections.forEach(function(section) {
+    clickAndScroll("#nav-" + section, "#" + section + "-section");
+  });
+}
+
+/*
+* Smoothens scrolling when About or Contact are selected from the navbar
+*/
+function clickAndScroll(link, section) {
+  var delay = 700;
+
+  $(link).click(function() {
+    $("html, body").animate({
+      scrollTop: $(section).offset().top
+    }, delay);
+
+    return false;
+  });
+}
