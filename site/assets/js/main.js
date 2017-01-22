f2404d6e3b0c11aa7101886b57105f6da3477c5e
Jonathan Grider <jagrider@purdue.edu>
2017-01-22 02:21:52 -0500

    update - change to search error

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -3,14 +3,5 @@
 */
 $(function()) {
   //TODO: FIND THINGS TO DO
-  var form = document.getElementById('formID');
-  form.noValidate = true;
-  form.addEventListener('submit', function(event)) {
-    if (!event.target.checkValidity()) {
-      event.preventDefault();
-      document.getElementById('errorMessageDiv').style.display = 'block';
-    }
-
-  }
 
 });
