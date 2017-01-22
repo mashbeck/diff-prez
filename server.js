c31101f99eb031025c7ac7ef7ee359af8bc01020
Matthew <mashbeck@purdue.edu>
2017-01-22 01:37:47 -0500

    Server now interacts with webpage

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var express = require('express');
+var bodyparser = require('body-parser');
 var app = express();
 
+app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static('site'));
+
+app.post('/search', function(req, res){
+	res.send('You searched for "' + req.body.searchbox + '"');
+});
 
 var server = app.listen(3000, function() {
 	console.log('Running server on port 3000');
